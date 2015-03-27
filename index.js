96ab166b2c4763b667a33f0427182bf1785c0215
Lin Clark <lin.w.clark@gmail.com>
2015-03-27 18:23:51 -0400

    prevent race condition

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ function plugin(options) {
   return function(files, metalsmith, done) {
     fs.readdir(metalsmith.path(options.directory), function(err,files){
       if(err) throw err;
+      
       files.forEach(function(file){
         var templateName = file.split('.').shift();
         var path = metalsmith.path(options.directory, file)
         var partialContents = fs.readFileSync(path).toString('utf8');
         handlebars.registerPartial(templateName, partialContents);
       });
-    });
 
-    done();
+      done();
+    });
   };
 }
